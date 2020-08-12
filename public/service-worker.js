57fade624c30c2c24dfbb798468c8536b53eb343
Videl90 <lorena.sa.0117@gmail.com>
2020-08-11 22:18:50 -0500

    app is now running offline

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,11 +1,12 @@
 const FILES_TO_CACHE = [
     '/',
-    './index.html',
-    './index.js',
-    './manifest.webmanifest',
-    './styles.css',
-    './icons/icon-192x192.png',
-    './icons/icon-512x512.png',
+    '/index.html',
+    '/index.js',
+    '/styles.css',
+    '/db.js',
+    '/manifest.webmanifest',
+    '/icons/icon-192x192.png',
+    '/icons/icon-512x512.png',
     "https://cdn.jsdelivr.net/npm/chart.js@2.8.0",
     "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
   ];
